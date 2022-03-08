a17c86755e5d92d6a5e4d070b4f52dbcf980f2b3
Technomann <marci.reiter501@gmail.com>
2022-03-08 18:33:34 +0100

    HF2

---
+++ static/js/coverflow.js
@@ -0,0 +1,9 @@
+(function(){
+    const prev = document.getElementsByClassName('gallery-controls-previous');
+    prev[0].classList.add('btn');
+    prev[0].classList.add('btn-darks');
+
+    const next = document.getElementsByClassName('gallery-controls-next');
+    next[0].classList.add('btn');
+    next[0].classList.add('btn-darks');
+})();
---
+++ static/js/file_upload.js
@@ -0,0 +1,12 @@
+(function(){
+    const uploadButton = document.getElementById('upload-input');
+    const fileChosen = document.getElementById('upload-text');
+    const uploadContainer = document.getElementById('upload-container');
+    const uploadImage = document.getElementById('upload-photo');
+    
+    uploadButton.addEventListener('change', 
+        function(){
+            fileChosen.textContent = this.files[0].name;
+        }
+    );
+})();
