153027052a137bdca3e0fe83ca6aede5f733eee7
Technomann <marci.reiter501@gmail.com>
2022-03-09 09:30:01 +0100

    HF2

--- static/js/coverflow.js
+++
@@ -1,9 +0,0 @@
-(function(){
-    const prev = document.getElementsByClassName('gallery-controls-previous');
-    prev[0].classList.add('btn');
-    prev[0].classList.add('btn-darks');
-
-    const next = document.getElementsByClassName('gallery-controls-next');
-    next[0].classList.add('btn');
-    next[0].classList.add('btn-darks');
-})();
