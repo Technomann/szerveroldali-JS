591f7cdf3e83eaa5a104960b520e4bac90fb6ae1
Technomann <marci.reiter501@gmail.com>
2022-03-27 17:37:33 +0200

    HF4

--- static/js/compare.js
+++ static/js/compare.js
@@ -2,12 +2,34 @@ let comparableSpacecraftIds = [];
 
 function compare(){
     if(comparableSpacecraftIds.length < 2){
-        //make error visible
+        const errorMsg = document.getElementById('compare-error-message');
+        errorMsg.classList.remove('hidden');
+        errorMsg.classList.add('block');
+    }else{
+        const Http = new XMLHttpRequest();
+        const url='/compare/' + comparableSpacecraftIds[0] + '/' + comparableSpacecraftIds[1];
+        Http.open("GET", url);
+        Http.send();
+
+        Http.onreadystatechange = (e) => {
+            console.log(Http.responseText)
+        }
     }
 }
 
-function disableUncheckedBoxes(){
+function changeUncheckedBoxesBehaviour(nable){
+    const checkBoxes = ducument.querySelectorAll('input[type=checkbox]');
+    const uncheckedBoxes = Array.from(checkBoxes)
+    .filter(i => !i.checked);
 
+    for(key in uncheckedBoxes){
+        let ub = uncheckedBoxes[key];
+        if(enable){
+            ub.disabled = false;
+        }else{ 
+            ub.disabled = true;
+        }
+    }
 }
 
 (function(){
@@ -20,9 +42,10 @@ function disableUncheckedBoxes(){
             .map(i => i.id);
 
             if(comparableSpacecraftIds.length = 2){
-                disableUncheckedBoxes();
+                changeUncheckedBoxesBehaviour(false);//disable
+            }else{
+                changeUncheckedBoxesBehaviour(true);//enable
             }
-            //disable unchecked ones
         });
     });
 
