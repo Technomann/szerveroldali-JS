78285ed8e36a92391f2de12df586765157a82188
Technomann <marci.reiter501@gmail.com>
2022-04-07 10:31:46 +0200

    HF6

--- middlewares/spacecraft/deleteSpacecraftMW.js
+++ middlewares/spacecraft/deleteSpacecraftMW.js
@@ -2,15 +2,34 @@
 /**
  * Deletes spacecraft from db by id and redirects to /spacecrafts/list
  */
- const reuireOption = require('../utility/requireOption');
+const reuireOption = require('../utility/requireOption');
+const async = require('./async/eachSeries');
 
  module.exports = function(objectRepository){
-     return function(req, res, next){
+     return async function(req, res, next){
         if(typeof res.locals.spacecraft === 'undefined' || typeof res.locals.comments === 'undefined'){
             return next();
         }
-        
-        res.locals.comments.forEach(item => {
+
+        await async.eachSeries(res.locals.comments, (comment) =>{
+            comment.remove((err) => {
+                res.error.message = 'Comment deletion not succesfull.'
+                res.error.code = '670';
+                res.redirect('/error');
+            })
+        }, _ => {
+            res.locals.spacecraft.remove((err) => {
+                if(err){
+                    res.error.message = 'Spacecraft deletion not succesfull.'
+                    res.error.code = '669';
+                    res.redirect('/error');
+                }
+            });
+        });
+
+        res.redirect('/list');
+
+        /*res.locals.comments.forEach(item => {
             item.remove((err) => {
                 res.error.message = 'Spacecraft deletion not succesfull.'
                 res.error.code = '670';
@@ -23,8 +42,6 @@
             res.error.message = 'Spacecraft deletion not succesfull.'
             res.error.code = '669';
             res.redirect('/error');
-        });
-
-        res.redirect('/list');
+        });*/
      };
  };
