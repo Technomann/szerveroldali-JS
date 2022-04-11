75eb172452bcb2fa2a35f149d4e76d91faa3b788
Technomann <marci.reiter501@gmail.com>
2022-04-11 08:58:03 +0200

    HF6

--- middlewares/spacecraft/deleteSpacecraftMW.js
+++ middlewares/spacecraft/deleteSpacecraftMW.js
@@ -3,21 +3,30 @@
  * Deletes spacecraft from db by id and redirects to /spacecrafts/list
  */
 const reuireOption = require('../utility/requireOption');
-const async = require('./async/eachSeries');
+const async = require('./async');
+
+
 
  module.exports = function(objectRepository){
-     return async function(req, res, next){
-        if(typeof res.locals.spacecraft === 'undefined' || typeof res.locals.comments === 'undefined'){
-            return next();
-        }
+    return async function(req, res, next){
+    if(typeof res.locals.spacecraft === 'undefined' || typeof res.locals.comments === 'undefined'){
+        return next();
+    }
 
-        await async.eachSeries(res.locals.comments, (comment) =>{
-            comment.remove((err) => {
+    await async.eachSeries(res.locals.comments, (comment) =>{
+        comment.remove((err) => {
+            if(err){
                 res.error.message = 'Comment deletion not succesfull.'
-                res.error.code = '670';
+                res.error.code = '669';
                 res.redirect('/error');
-            })
-        }, _ => {
+            }
+        });
+    }, (err) => {
+        if(err){
+            res.error.message = 'Comment deletion not succesfull.'
+                res.error.code = '677';
+                res.redirect('/error');
+        }else{
             res.locals.spacecraft.remove((err) => {
                 if(err){
                     res.error.message = 'Spacecraft deletion not succesfull.'
@@ -25,23 +34,9 @@ const async = require('./async/eachSeries');
                     res.redirect('/error');
                 }
             });
-        });
+        }
+    });
 
-        res.redirect('/list');
-
-        /*res.locals.comments.forEach(item => {
-            item.remove((err) => {
-                res.error.message = 'Spacecraft deletion not succesfull.'
-                res.error.code = '670';
-                res.redirect('/error');
-            });
-            
-        });
-
-        res.locals.spacecraft.remove((err) => {
-            res.error.message = 'Spacecraft deletion not succesfull.'
-            res.error.code = '669';
-            res.redirect('/error');
-        });*/
-     };
+    res.redirect('/list');
+    };
  };
