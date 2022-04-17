82911ff6574fe25332a06ee6d8044cb25db386c0
Technomann <marci.reiter501@gmail.com>
2022-04-17 11:38:31 +0200

    HF6

--- middlewares/comment/getCommentsMW.js
+++ middlewares/comment/getCommentsMW.js
@@ -3,19 +3,20 @@
  * Get all comments for specific spacecraft or spacecrafts by spacecraftId and puts them into locals and calls next
  */
  const requireOption = require('../utility/requireOption');
+ const mongoose = require('mongoose');
 
  module.exports = function(objectRepository){
     const CommentModel = requireOption(objectRepository, 'CommentModel');
     
     return function(req, res, next){
-        CommentModel.find({_id: res.locals.spacecraft._id}, (err, comments) => {
+        CommentModel.find({spacecraft: mongoose.Types.ObjectId(res.locals.spacecraft._id)}, (err, comments) => {
             if(err){
                 res.locals.error.code = '988';
                 res.locals.error.message = 'Cannot get comment.';
                 res.redirect('/error');
             }
 
-            res.locals.comments = comments;
+            res.locals.spacecraft.comments = comments;
             return next();
         });
     };
--- middlewares/comment/saveCommentMW.js
+++ middlewares/comment/saveCommentMW.js
@@ -10,10 +10,12 @@ const async = require('async');
     const CommentModel = requireOption(objectRepository, 'CommentModel');
 
     return function(req, res, next){
-        if(typeof req.body === 'undefined'){
+        //HTTP GET
+        if(req.method === 'GET'){
             return next();
         }
 
+        //USER SENDS NOT ENOUGH DATA
         if((typeof req.body.title === 'undefined') || 
         (typeof req.body.text === 'undefined') ||
         (typeof req.body.rating === 'undefined')){
@@ -21,52 +23,62 @@ const async = require('async');
             return next();
         }
 
-        if(typeof res.locals.comment === 'undefined'){
-            res.locals.comment = new CommentModel();
-        }
-
-        res.locals.comment.title = req.body.title;
-        res.locals.comment.text = req.body.text;
-
+        //RATING DOMAIN NOT CORRECT
         if(req.body.rating > 5 || req.body.rating < 1){
             res.locals.error.message = 'Rating must be from 1 to 5!';
             return next();
         }
 
-        if(req.body.title.length < 3|| req.body.text.length < 3){
+        //TITLE OR TEST NOT LONG ENOUGH
+        if(req.body.title.length < 3 || req.body.text.length < 3){
             res.locals.error.message = 'Title and comment must be at least 3 characters long! ';
             return next();
         }
 
+        //IF ITS NEW COMMENT -> CREATE NEW ONE
+        if(typeof res.locals.comment === 'undefined'){
+            res.locals.comment = new CommentModel();
+        }
+
+        //PROVIDE COMMENT DATA
+        res.locals.comment.title = req.body.title;
+        res.locals.comment.text = req.body.text;
         res.locals.comment.date = Date.now();
         res.locals.comment.author = res.locals.loggedInUser;
         res.locals.comment.spacecraft = res.locals.spacecraft;
 
+        //ADJUST SPACECRAFT RATING
         res.locals.spacecraft.ratingSum += req.body.rating;
         res.locals.spacecraft.ratingAmount++;
 
+        //SAVE SPACECRAFT AND COMMENT PARALLEL
         async.parallel([
-            res.locals.comment.save((err) => {
-                if(err){
-                    res.locals.error.code = '777';
-                    res.locals.error.message = 'Cannot save comment to DB.';
-                    return res.redirect('/error');
-                }
-            }), 
-            res.locals.spacecraft.save((err) => {
-                if(err){
-                    res.locals.error.code = '779';
-                    res.locals.error.message = 'Cannot save spaceceaft to DB.';
-                    return res.redirect('/error');
-                }
-            })
+            (callback) => {
+                res.locals.comment.save((err) => {
+                    if(err){
+                        res.locals.error.code = '777';
+                        res.locals.error.message = 'Cannot save comment to DB.';
+                        return res.redirect('/error');
+                    }
+                    callback(err);
+                })
+            },
+            (callback) => {
+                res.locals.spacecraft.save((err) => {
+                    if(err){
+                        res.locals.error.code = '779';
+                        res.locals.error.message = 'Cannot save spaceceaft to DB.';
+                        return res.redirect('/error');
+                    }
+                    callback(err);
+                })
+            } 
         ], (err) => {
             if(err){
                 res.locals.error.code = '778';
                 res.locals.error.message = 'Cannot save values into DB.';
                 return res.redirect('/error');
             }
-
             return res.redirect('/spacecraft/' + res.locals.spacecraft._id + '/details');
         });
     };
